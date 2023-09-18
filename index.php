4a427928073ace56791e44b1f7379fb75e9fd2c8
Yana Karpliuk <yana.karpliuk@lembergsolutions.com>
2023-09-18 13:03:23 +0300

    Update text

--- index.php
+++ index.php
@@ -44,7 +44,7 @@ if (isset($_POST['submit'])) {
 
 <img src="/img/logo.png" class="w-25 mb-3" alt="">
 <h2>Feedback</h2>
-<p class="lead text-center">Leave feedback for Traversy Media</p>
+<p class="lead text-center">Leave feedback for Feedback Project</p>
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST" class="mt-4 w-75">
     <div class="mb-3">
         <label for="name" class="form-label">Name</label>
