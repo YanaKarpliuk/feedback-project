609f483b85825b23931306d49ea4fb7bc75c25fa
Yana Karpliuk <yana.karpliuk@lembergsolutions.com>
2023-09-18 11:11:55 +0300

    Fetch data from table

--- feedback.php
+++ feedback.php
@@ -1,28 +1,26 @@
 <?php include 'inc/header.php' ?>
 
 <?php
-$feedback = [
-    [
-        'id' => 1,
-        'name' => 'yana',
-        'email' => 'email',
-        'body' => 'body'
-    ]
-]
+$sql = 'SELECT * FROM feedback_table';
+$result = $conn->query($sql);
 ?>
 
     <h2>Feedback</h2>
 
-<?php if (empty($feedback)): ?>
+<?php if (empty($result)): ?>
     <p class="lead mt3">There is no feedback</p>
 <?php endif; ?>
 
-<?php foreach ($feedback as $item): ?>
+<?php foreach ($result as $item): ?>
     <div class="card my-3 w-75">
         <div class="card-body text-center">
             <?php echo $item['body']; ?>
             <div class="text-secondary mt2">
                 By <?php echo $item['name']; ?>
+                on <?php
+                $date=date_create($item['date']);
+                echo date_format($date, "M j, Y");
+                ?>
             </div>
         </div>
     </div>
